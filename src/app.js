64dab63e34987745d8e4fef7236c78f7149b6630
guias12 <guilherme.p.assis@gmail.com>
2019-12-30 21:06:15 -0300

    refactor in the js code to make it more modern, in the future ill make the css better too....

--- src/app.js
+++ src/app.js
@@ -1,30 +1,33 @@
 function myFunction(){          
-	var rand = Math.floor((Math.random() * (151 - 1) + 1));
-    var shinyChance = Math.floor((Math.random() * (500 - 1) + 1));
-    var link = 'https://pokeapi.co/api/v2/pokemon/' + rand + '/';          
-    axios.get(link.toString()).then(function(response){          
-    	var x = document.createElement("IMG");
-    	x.setAttribute("width", "304");
-    	x.setAttribute("height", "304");
-    	x.setAttribute("class", "image")
-    	if(Math.floor((shinyChance/2)) <= 50){
-        	if(response.data.sprites.front_shiny !== null){
-            	x.setAttribute("src", response.data.sprites.front_shiny);					    
-					document.body.appendChild(x);
-			    } else {
-					x.setAttribute("src", response.data.sprites.front_default);					
-				    document.body.appendChild(x);	
-			    }
+	let rand = Math.floor((Math.random() * (151 - 1) + 1));
+    let shinyChance = Math.floor((Math.random() * (500 - 1) + 1));
+	let url = `https://pokeapi.co/api/v2/pokemon/${ rand }/`;
+
+    axios.get(url).then(response =>{
+		let img = document.createElement("IMG");
+    	img.setAttribute("width", "304");
+    	img.setAttribute("height", "304");
+    	img.setAttribute("class", "image");
+    	if(Math.floor((shinyChance/2)) <= 50) {
+        	if(response.data.sprites.front_shiny !== null) {
+            	img.setAttribute("src", response.data.sprites.front_shiny);					    
+				document.body.appendChild(img);
+			} else {
+				img.setAttribute("src", response.data.sprites.front_default);					
+			    document.body.appendChild(img);	
+			}
       	} else {
-			x.setAttribute("src", response.data.sprites.front_default);
-			document.body.appendChild(x);
+			img.setAttribute("src", response.data.sprites.front_default);
+			document.body.appendChild(img);
 		}
-		var para = document.createElement("P");
+		let para = document.createElement("P");
       	para.setAttribute("class", "paragraph");
-		var t = document.createTextNode("A wild " + response.data.name.toUpperCase() + ' appeared!');
+		let t = document.createTextNode(`A wild ${ response.data.name.toUpperCase() } appeared!`);
 		para.appendChild(t);
 		document.body.appendChild(para);
-    }).catch(function (error){
-      console.log('deu ruim');
-    })
+	}).catch(error => {
+		console.log(`Somenthing went wrong`)
+	})		
+    	
+    
   }
